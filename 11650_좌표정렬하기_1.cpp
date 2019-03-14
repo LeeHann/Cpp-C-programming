056f256ca11f417b33719b90b78a72c70c74619d
LeeHann <44077831+LeeHann@users.noreply.github.com>
2019-03-15 00:25:11 +0900

    Add files via upload

---
+++ 11650_좌표정렬하기_1.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct pnt
+{
+	int x;
+	int y;
+};
+struct pnt xy[100001];
+bool comp (const pnt&a, const pnt&b)
+{
+	if (a.x == b.x)
+	{
+		return a.y < b.y;
+	}
+	return a.x < b.x;
+}
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	for (int i=0; i<n; i++) scanf("%d%d", &xy[i].x, &xy[i].y);
+	sort(xy, xy+n, comp);
+	for (int i=0; i<n; i++) printf("%d %d\n", xy[i].x, xy[i].y);
+	return 0;
+}
