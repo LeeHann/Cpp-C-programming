174be5fc665501682b0507f19fe5db81b9ff9b14
LeeHann <44077831+LeeHann@users.noreply.github.com>
2019-03-25 14:46:43 +0900

    Add files via upload

--- absolute_98_5.cpp
+++ absolute_98_5.cpp
@@ -1,11 +1,7 @@
 #include <iostream>
-#include <string.h>
 using namespace std;
-
 int main()
 {
-	char sen[21] = "TOO+TOO+TOO+TOO=GOOD";
-	int t=0,o=0,g=0,d=0;
 		for(int l=0; l<10; l++)
 		{
 			for(int k=0; k<10; k++)
@@ -14,15 +10,14 @@ int main()
 				{
 					for(int i=0; i<10; i++)
 					{
-						t=i;
-						if (t*100*4+o*10*4+o*4!=g*1000+o*100+o*10+d) break;
+						if (i==0|| i==j || i==k || i==l || j==k || j==l || k==l) continue;
+						if (i*100*4+j*10*4+j*4==k*1000+j*100+j*10+l) 
+						{
+							printf("T=%d O=%d G=%d D=%d", i, j, k, l);
+							return 0;
+						}
 					}
-					o=j;
 				}
-				g=k;
 			}
-			d=l;
 		}
-	printf("T=%d O=%d G=%d D=%d", t, o, g, d);
-	return 0;
 }
