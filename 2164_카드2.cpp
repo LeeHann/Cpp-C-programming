20d041b81f29f10a46ecffb1c268eb0c06e32a0e
LeeHann <44077831+LeeHann@users.noreply.github.com>
2020-03-19 22:56:06 +0900

    Add files via upload

---
+++ 2164_카드2.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;
+	queue<int> card;
+	cin >> n;
+	
+	for(int i=1; i<=n; i++)
+		card.push(i);
+	
+	while(card.size()>1){
+		card.pop();
+		card.push(card.front());
+		card.pop();	
+	}
+	printf("%d\n", card.front());
+	return 0;
+}
