afdb9e5d049bf51898d36ace453cf4f76e47ea0e
LeeHann <0104ihn@naver.com>
2021-09-10 00:27:38 +0900

    소수 클리어!!!

--- acmicpc/2581_소수.c
+++ acmicpc/2581_소수.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
 
+int arr[10001];
+
 int main()
 {
-	
+	int m, n, i, j, sum, min;
+	sum = 0;
+	min = 0;
+	scanf("%d %d", &m, &n);
+	arr[1] = 1;
+	for(i = 2; i < n; i++)
+	{
+		for(j = 2; i*j <= 10000; j++)
+		{
+			arr[i*j] = 1;
+		}
+	}
+	for(i = m; i <= n; i++)
+	{
+		if (arr[i] == 0)
+		{
+			if (min == 0) min = i;
+			sum += i;
+		}
+	}
+	if (sum == 0) printf("-1\n");
+	else
+		printf("%d\n%d\n", sum, min);
 	return (0);
 }
